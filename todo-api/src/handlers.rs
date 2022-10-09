8ce1bb95d129c4ccc2715da1b279670c0e56d7e0
ytakasugi <sh7.tibi0129@gmail.com>
2022-10-10 00:17:55 +0900

    change files

--- todo-api/src/handlers.rs
+++ todo-api/src/handlers.rs
@@ -1,15 +1,56 @@
 use axum::{
-    extract::Extension,
+    extract::{Extension, Path},
     http::StatusCode,
     response::IntoResponse,
     Json
 };
 use std::sync::Arc;
 
-use crate::repositories::{CreateTodo, TodoRepository};
+use crate::repositories::{CreateTodo, TodoRepository, UpdateTodo};
 
-pub async fn create_todo<T: TodoRepository>(Json(payload): Json<CreateTodo>, Extension(repository): Extension<Arc<T>>) -> impl IntoResponse {
+pub async fn create_todo<T: TodoRepository>(
+    Json(payload): Json<CreateTodo>,
+    Extension(repository): Extension<Arc<T>>
+) -> impl IntoResponse {
     let todo = repository.create(payload);
 
     (StatusCode::CREATED, Json(todo))
 }
+
+pub async fn find_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository.find(id).ok_or(StatusCode::NOT_FOUND)?;
+    Ok((StatusCode::OK, Json(todo)))
+}
+
+pub async fn all_todo<T: TodoRepository>(
+    Extension(repository): Extension<Arc<T>>
+) -> impl IntoResponse {
+    let todo = repository.all();
+    (StatusCode::OK, Json(todo))
+}
+
+pub async fn update_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Json(payload): Json<UpdateTodo>,
+    Extension(repository): Extension<Arc<T>>
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository
+        .update(id, payload)
+        .or(Err(StatusCode::NOT_FOUND))?;
+
+    Ok((StatusCode::OK, Json(todo)))
+}
+
+pub async fn delete_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>
+) -> StatusCode {
+    repository
+        .delete(id)
+        .map(|_| StatusCode::NO_CONTENT)
+        .unwrap_or(StatusCode::NOT_FOUND)
+}
+
--- todo-api/src/repositories.rs
+++ todo-api/src/repositories.rs
@@ -32,6 +32,13 @@ pub struct CreateTodo {
     text: String,
 }
 
+#[cfg(test)]
+impl CreateTodo {
+    pub fn new(text: String) -> Self {
+        Self { text }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct UpdateTodo {
     text: Option<String>,
